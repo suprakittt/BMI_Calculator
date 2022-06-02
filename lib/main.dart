92fc998c8699e5a8161f244ac2658f5ac31279b6
suprakit <suprakitonnuch@gmail.com>
2022-06-02 23:04:49 +0700

    complete the main page

--- lib/main.dart
+++ lib/main.dart
@@ -23,62 +23,98 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _heightController = TextEditingController();
-  final TextEditingController _weightController = TextEditingController();
-  double? _result;
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+  double? _bmi;
+  String _message = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[100],
-      appBar: AppBar(
-        title: Text('BMI Calculator'),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-      ),
-      body: Column(children: <Widget>[
-        Text(
-          'Height (cm)',
+        backgroundColor: Colors.blue[100],
+        appBar: AppBar(
+          title: Text('BMI Calculator'),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
         ),
-        TextField(
-          decoration: InputDecoration(
-            icon: Icon(Icons.trending_up),
+        body: Center(
+          child: Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(image: AssetImage("photo/pexels-pixabay-255501.jpg"),
+                    fit: BoxFit.cover)
+            ),
+            width: 350,
+            child: Card(
+              color: Colors.white,
+              elevation: 10,
+              child: Padding(
+                padding: const EdgeInsets.all(100),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+
+                    TextField(
+                      keyboardType:
+                      TextInputType.numberWithOptions(decimal: true),
+                      decoration: InputDecoration(labelText: 'Height (cm)'),
+                      controller: _heightController,
+                    ),
+                    TextField(
+                      keyboardType:
+                      TextInputType.numberWithOptions(decimal: true),
+                      decoration: InputDecoration(
+                        labelText: 'Weight (kg)',
+                      ),
+                      controller: _weightController,
+                    ),
+                    RaisedButton(
+                      shape: StadiumBorder(),
+                      color: Colors.greenAccent,
+                      onPressed: calculate,
+                      child: Text('Calculate'),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      child: Text(
+                        _bmi == null ? '' : _bmi!.toStringAsFixed(2),
+                        style: TextStyle(fontSize: 25),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      child: Text(
+                        _message,
+                        style: TextStyle(fontSize: 20, color: Colors.grey.withOpacity(1)),
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
-        SizedBox(height: 20),
-        Text(
-          'weight (kg)',
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()),
+            );
+          },
+          child: Icon(Icons.flatware),
+          backgroundColor: Colors.green,
         ),
-        TextField(
-          decoration: InputDecoration(
-//              labelText: 'weight (kg)',
-            icon: Icon(Icons.line_weight),
-          ),
-        ),
-        SizedBox(height: 15),
-        RaisedButton(
-          color: Colors.deepPurple,
-          child: Text(
-            "Calculate",
-            style: TextStyle(color: Colors.white),
-          ),
-          onPressed: calculate,
-        ),
-        SizedBox(height: 15),
-        Text(
-          _result == null ? "BMI" : "${_result?.toStringAsFixed}",
-          style: TextStyle(
-            color: Colors.redAccent,
-            fontSize: 19.4,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-      ]),
     );
   }
 
@@ -88,7 +124,45 @@ class _MyHomePageState extends State<MyHomePage> {
 
     double heightSquare = height * height;
     double result = weight / heightSquare;
-    _result = result;
-    setState(() {});
+    _bmi = result;
+    setState(() {
+      if (_bmi! < 18.5) {
+        _message = "You are underweight";
+      } else if (_bmi! < 25) {
+        _message = 'You body is fine';
+      } else if (_bmi! < 30) {
+        _message = 'You are overweight';
+      } else {
+        _message = 'You are obese';
+      }
+    });
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Healthy Food Recommendation'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Icon(Icons.home_filled),
+        backgroundColor: Colors.green,
+      ),
+    );
   }
 }
