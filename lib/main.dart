2b2c31ec389814cda8daa704f41386053e5cd810
suprakit <suprakitonnuch@gmail.com>
2022-06-04 19:27:05 +0700

    added recipes

--- lib/main.dart
+++ lib/main.dart
@@ -126,8 +126,8 @@ class _MyHomePageState extends State<MyHomePage> {
       },
     );
     AlertDialog alert = AlertDialog(
-      title: Text("Your weight is greater than what is considered healthy."),
-      content: Text("Please Click the green button below to see healthy food recipes."),
+      title: Text("WARNING !"),
+      content: Text("Your weight is greater than what is considered healthy. \nPlease Click the green button below to see healthy food recipes."),
       actions: [
         okButton,
       ],
@@ -171,20 +171,21 @@ class PhotoItem {
 class SecondRoute extends StatelessWidget {
 
   final List<PhotoItem> images = [
-    PhotoItem("photo/1.jpg", "Chicken Satay salad"),
-    PhotoItem("photo/2.jpg", "Linguine with avocado"),
-    PhotoItem("photo/3.jpg", "Pork souvlaki"),
-    PhotoItem("photo/4.jpg", "Salsa verde"),
-    PhotoItem("photo/5.jpg", "Roasted cod"),
-    PhotoItem("photo/6.jpg", "Pomegranate"),
-    PhotoItem("photo/7.jpg", "Tuna Spaghetti"),
-    PhotoItem("photo/8.jpg", "Turmeric, fish curry"),
-    PhotoItem("photo/9.jpg", "Crispy grilled feta"),
+    PhotoItem("photo/1.jpg", "Banana & tahini porridge\n\n- 1tbsp tamari\n- 1 tbsp tahini\n- 150ml milk of your choice, plus 1 tbsp\n- 100g porridge oats\n- 2 small bananas , sliced\n- seeds from 2 cardamom pods, crushed\n- 1 tbsp toasted sesame seeds"),
+    PhotoItem("photo/2.jpg", "Overnight oats\n\n- ¼ tsp ground cinnamon\n- 50g rolled porridge oats\n- 2 tbsp natural yogurt\n- 50g mixed berries\n  drizzle of honey\n- ½ tbsp nut butter "),
+    PhotoItem("photo/3.jpg", "Burrito\n\n- 1 tsp chipotle paste\n- 1 egg\n- 1 tsp rapeseed oil\n- 50g kale\n- 7 cherry tomatoes, halved\n- ½ small avocado, sliced\n- 1 wholemeal tortilla wrap, warmed"),
+    PhotoItem("photo/4.jpg", "Healthy pancakes\n\n- 50g self-raising flour\n- 50g wholemeal or wholegrain flour\n- 2 small eggs, separated\n- 150ml skimmed milk\n- berries and low-fat yogurt or fromage frais to serve"),
+    PhotoItem("photo/5.jpg", "Slow cooker bio yogurt\n\n- 2l whole milk\n- 100ml live yogurt , either shop bought or from a previous homemade batch"),
+    PhotoItem("photo/6.jpg", "Baked banana porridge\n\n- 2 small bananas , halved lengthways\n- 100g jumbo porridge oats\n- ¼ tsp cinnamon\n- 150ml milk of your choice, plus extra to serve\n- 4 walnuts , roughly chopped"),
+    PhotoItem("photo/7.jpg", "Green fritters\n\n- 140g courgettes , grated\n- 3 medium eggs\n- 85g broccoli florets, finely chopped\n- small pack dill , roughly chopped\n- 3 tbsp gluten-free flour or rice flour\n- 2 tbsp sunflower oil , for frying"),
+    PhotoItem("photo/8.jpg", "Saucy bean baked eggs\n\n- 2 x 400g cans cherry tomatoes\n- 400g can mixed bean salad , drained\n- 200g baby spinach\n- 4 medium eggs\n- 50g thinly sliced smoked ham , torn"),
+    PhotoItem("photo/9.jpg", "Avocado & strawberry smoothie\n\n- ½ avocado , stoned, peeled and cut into chunks\n- 150g strawberry , halved\n- 4 tbsp low-fat natural yogurt\n- 200ml semi-skimmed milk\n- lemon or lime juice , to taste\n- honey , to taste"),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue[50],
       appBar: AppBar(
           title: Text("Healthy Food Recommend"),
           backgroundColor: Colors.blue,
@@ -240,10 +241,12 @@ class RouteTwo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.blue[50],
       appBar: AppBar(
-        title: Text('Recieves'),
+        title: Text(''),
       ),
-      body: Column(
+      body: Center(
+      child: ListView(
         children: [
           AspectRatio(
             aspectRatio: 1,
@@ -252,18 +255,35 @@ class RouteTwo extends StatelessWidget {
               child: Image(
                 image: AssetImage(image),
               ),
+              decoration: BoxDecoration(
+                  color: Colors.blue[100]),
+              margin: const EdgeInsets.all(10.0) ,
             ),
           ),
           Container(
-            margin: const EdgeInsets.all(20.0),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("photo/textbg.jpg"),
+                fit: BoxFit.fill,
+              ),
+            ),
+            margin: const EdgeInsets.all(40.0),
             child: Center(
               child: Text(
                 name,
-                style: TextStyle(fontSize: 40),
+                style: TextStyle(fontSize: 30),
               ),
             ),
           ),
         ],
+      ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Icon(Icons.arrow_back),
+        backgroundColor: Colors.green,
       ),
     );
   }
